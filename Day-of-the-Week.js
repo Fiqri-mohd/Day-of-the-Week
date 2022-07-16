963708728189312a40166fc52464542b117c4295
Fiqri <46043858+Fiqri-mohd@users.noreply.github.com>
2022-07-17 01:02:36 +0800

    Add files via upload

---
+++ Day-of-the-Week.js
@@ -0,0 +1,31 @@
+var day = "Sunday";
+
+switch (day)
+{
+    case("Monday"):
+        console.log("Eat");
+            break;
+
+    case("Tuesday"):
+        console.log("Drink");
+            break;
+
+    case("Wednesday"):
+        console.log("Sleep");
+            break;
+
+    case("Thursday"):
+    console.log("Play");
+             break;
+
+    case("Friday"):
+    console.log("Walk");
+            break;
+
+    case("Saturday"):
+    console.log("Run");
+            break;
+
+    default:
+    console.log("Sleep Walk");
+}
